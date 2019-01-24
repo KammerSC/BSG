8761be48fac5646b6506be54819be0c9fc322c5b
Norbert Gulácsi <Norbert_Gulacsi@epam.com>
2019-01-24 21:28:56 +0100

    base

--- Assets/Scripts/Classes/Board.cs
+++ Assets/Scripts/Classes/Board.cs
@@ -5,268 +5,101 @@ using UnityEngine;
 
 public class Board
 {
-    public Settings settings;
+    Settings settings;
 
-    public Board(Settings settings)
+    Board(Settings settings)
     {
         this.settings = settings;
     }
 
-    public int maxVipers = 8;
-    public int nextId = 0;
-
-    public List<Civil> civils = new List<Civil>();
-    public List<Viper> vipers = new List<Viper>();
-    public List<Raider> raiders = new List<Raider>();
-    public List<HeavyRaider> heavyRaiders = new List<HeavyRaider>();
-    public List<BaseStar> baseStars = new List<BaseStar>();
-    public int[] hajok = { civils, vipers, raiders, heavyRaiders, baseStars };
-
-    public void aktivalCylon(int hajo)
-    {
-        switch (hajo)
-        {
-            case 2:
-                foreach (Raider raider in raiders)
-                {
-                    raider.activate();
-                }
-            case 3:
-                foreach (HeavyRaider heavyRaider in heavyRaiders)
-                {
-                    heavyRaider.activate();
-                }
-            case 4:
-                foreach (BaseStar baseStar in baseStars)
-                {
-                    baseStar.activate();
-                }
-            default:
-                Console.WriteLine("Nincs ilyen cylon hajó típus");
-        }
-
-    }
-
-    civilHajokSzama()
-    {
-        int counter = 0;
-        for (int i = 0; i < 6; i++)
-        {
-            counter += civilSzamSzektor(i);
-        }
-        return counter;
-    }
-
-    public bool raiderSzamSzektor(int szektor)
-    {
-        return szektorok[szektor][2];
-    }
-    public bool viperaSzamSzektor(int szektor)
-    {
-        return szektorok[szektor][1];
-    }
-    public bool heavyRaiderSzamSzektor(int szektor)
-    {
-        return szektorok[szektor][3];
-    }
-    public bool civilSzamSzektor(int szektor)
-    {
-        return szektorok[szektor][0];
-    }
-    public bool baseStarSzamSzektor(int szektor)
-    {
-        return szektorok[szektor][4];
-    }
+    List<Civil> civils = new List<Civil>();
+    List<Viper> vipers = new List<Viper>();
+    List<Raider> raiders = new List<Raider>();
+    List<HeavyRaider> heavyRaiders = new List<HeavyRaider>();
+    List<BaseStar> baseStars = new List<BaseStar>();
 
     //0 civil, 1 vipera, 2 raider, 3 heavyRaider, 4 baseStar, (5 galactica, 6 nuke)
-    int[,] szektorok = new int[6, 4];
+    byte[,] szektorok = new byte[6, 5];
 
-    public void tamad(int tamadoTipus, int tamadottTipus)
+    void levesz(byte szektor, object hajo, byte index)
     {
-        //unity felületről jön
-        int tamadottAzonosito;
-        //teszt dobas
-        int dobas = 3;
-
-        hajok[tamadottTipus].Find(x => x.id == tamadottAzonosito).tamadjak(tamadoTipus);
+        if (hajo is Civil)
+            civils.RemoveAt(index);
+        else if (hajo is Viper)
+            vipers.RemoveAt(index);
+        else if (hajo is Raider)
+            raiders.RemoveAt(index);
+        else if (hajo is HeavyRaider)
+            heavyRaiders.RemoveAt(index);
+        else if (hajo is BaseStar)
+            baseStars.RemoveAt(index);
+        else
+            Console.WriteLine("No such ship type!");
     }
 
-    public class Raider
+    void lerak(byte szektor, object hajo)
     {
-        public byte position;
-        public byte id;
+        if (hajo is Civil)
+            civils.Add(new Civil(szektor));
+        else if (hajo is Viper)
+            vipers.Add(new Viper(szektor));
+        else if (hajo is Raider)
+            raiders.Add(new Raider(szektor));
+        else if (hajo is HeavyRaider)
+            heavyRaiders.Add(new HeavyRaider(szektor));
+        else if (hajo is BaseStar)
+            baseStars.Add(new BaseStar(szektor));
+        else
+            Console.WriteLine("No such ship type!");
+    }
 
-        public Raider(int szektor)
+    class Raider
+    {
+        byte szektor;
+
+        public Raider(byte szektor)
         {
-            position = szektor;
-            this.id = nextId++;
-        }
-
-        public bool activated;
-
-        public int tamadjak(int hajo, int dobas)
-        {
-            if (dobas > 2)
-                pusztul();
-        }
-
-        public void activate()
-        {
-            int raiderSzam = raiderSzamSzektor(szektor);
-            for (int i = 0; i < raiderSzam; i++)
-            {
-                if (viperaSzamSzektor(position))
-                {
-                    tamad(2, 1);
-                }
-                else if (civilSzamSzektor(position))
-                {
-                    raider.tamad(2, 0);
-                }
-                else if (civilHajokSzama())
-                {
-                    //mozog
-                }
-                else
-                    tamad(2, 5);
-            }
-        }
-
-        public pusztul()
-        {
-            raiders.Remove(raiders.Find(x => x.id == id));
+            this.szektor = szektor;
         }
     }
 
-    public class HeavyRaider
+    class HeavyRaider
     {
-        public byte position;
-        public byte id;
+        byte szektor;
 
-        public HeavyRaider(int szektor)
+        public HeavyRaider(byte szektor)
         {
-            position = szektor;
-            this.id = nextId++;
-        }
-
-        public bool activated;
-
-        public int tamadjak(int hajo, int dobas)
-        {
-            if (dobas > 6)
-                pusztul();
-        }
-
-        public pusztul()
-        {
-            heavyRaiders.Remove(raiders.Find(x => x.id == id));
+            this.szektor = szektor;
         }
     }
 
-    public class Viper
+    class Viper
     {
-        public byte position;
-        public byte id;
+        byte szektor;
 
-        public Viper(int szektor)
+        public Viper(byte szektor)
         {
-            position = szektor;
-            this.id = nextId++;
-        }
-
-        public byte player;
-        public bool activated;
-
-        public int tamadjak(int hajo, int dobas)
-        {
-            if (dobas == 8)
-                pusztul();
-            else if (dobas > 4)
-                return 1;
-            else
-                return 0;
-        }
-
-        public pusztul()
-        {
-            vipers.Remove(raiders.Find(x => x.id == id));
-            maxVipers--;
-        }
-        public serul()
-        {
-            //
+            this.szektor = szektor;
         }
     }
 
-    public class Civil
+    class Civil
     {
-        public byte position;
-        public byte id;
+        byte szektor;
 
-        public Civil(int szektor)
+        public Civil(byte szektor)
         {
-            position = szektor;
-            this.id = nextId++;
-        }
-
-        public bool activated;
-
-        public int tamadjak(int hajo, int dobas)
-        {
-
-            civils.Remove(raiders.Find(x => x.id == id));
-            //TODO hatás
+            this.szektor = szektor;
         }
     }
 
-    public class BaseStar
+    class BaseStar
     {
-        public byte position;
-        public byte id;
+        byte szektor;
 
-        public BaseStar(int szektor)
+        public BaseStar(byte szektor)
         {
-            position = szektor;
-            this.id = nextId++;
-        }
-
-        public bool activated;
-
-        public int tamadjak(int hajo, int dobas)
-        {
-            switch (hajo)
-            {
-                case 1:
-                    if (dobas == 8)
-                        return 1;
-                    else
-                        return 0;
-                case 5:
-                    if (dobas > 4)
-                        return 1;
-                    else
-                        return 0;
-                case 6:
-                    if (dobas > 6)
-                    {
-                        pusztul(true);
-                        return 2;
-                    }
-                    else if (dobas > 2)
-                        pusztul(false);
-                    else
-                        //TODO kétszer sérül
-                        return 1;
-            }
-        }
-
-        public pusztul(bool areaDamage)
-        {
-            baseStars.Remove(raiders.Find(x => x.id == id));
-        }
-        public serul()
-        {
-            //
+            this.szektor = szektor;
         }
     }
 }
