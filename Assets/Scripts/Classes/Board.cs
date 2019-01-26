0b4e79acc4003e0760017e8e808e408a1893bfdd
Norbert Gulácsi <Norbert_Gulacsi@epam.com>
2019-01-26 15:59:59 +0100

    fix

--- Assets/Scripts/Classes/Board.cs
+++ Assets/Scripts/Classes/Board.cs
@@ -12,42 +12,49 @@ public class Board
         this.settings = settings;
     }
 
+    //Ship types should be referred in methods by numbers as follows
+    //0 civil, 1 vipera, 2 raider, 3 heavyRaider, 4 baseStar
     List<Civil> civils = new List<Civil>();
     List<Viper> vipers = new List<Viper>();
     List<Raider> raiders = new List<Raider>();
     List<HeavyRaider> heavyRaiders = new List<HeavyRaider>();
     List<BaseStar> baseStars = new List<BaseStar>();
 
-    //0 civil, 1 vipera, 2 raider, 3 heavyRaider, 4 baseStar, (5 galactica, 6 nuke)
     byte[,] szektorok = new byte[6, 5];
 
-    void levesz(byte szektor, object hajo, byte index)
+    void leveszRaider(byte szektor, byte type)
     {
-        if (hajo is Civil)
-            civils.RemoveAt(index);
-        else if (hajo is Viper)
-            vipers.RemoveAt(index);
-        else if (hajo is Raider)
-            raiders.RemoveAt(index);
-        else if (hajo is HeavyRaider)
-            heavyRaiders.RemoveAt(index);
-        else if (hajo is BaseStar)
-            baseStars.RemoveAt(index);
+        if (type == 2)
+            raiders.Remove(raiders.Find(x => x.szektor == szektor));
+        else if (type == 3)
+            heavyRaiders.Remove(heavyRaiders.Find(x => x.szektor == szektor));
         else
             Console.WriteLine("No such ship type!");
     }
 
-    void lerak(byte szektor, object hajo)
+    void levesz(object hajo)
     {
         if (hajo is Civil)
-            civils.Add(new Civil(szektor));
+            civils.Remove((Civil)hajo);
         else if (hajo is Viper)
-            vipers.Add(new Viper(szektor));
-        else if (hajo is Raider)
-            raiders.Add(new Raider(szektor));
-        else if (hajo is HeavyRaider)
-            heavyRaiders.Add(new HeavyRaider(szektor));
+            vipers.Remove((Viper)hajo);
         else if (hajo is BaseStar)
+            baseStars.Remove((BaseStar)hajo);
+        else
+            Console.WriteLine("No such ship type!");
+    }
+
+    void lerak(byte szektor, byte type)
+    {
+        if (type == 0)
+            civils.Add(new Civil(szektor));
+        else if (type == 1)
+            vipers.Add(new Viper(szektor));
+        else if (type == 2)
+            raiders.Add(new Raider(szektor));
+        else if (type == 3)
+            heavyRaiders.Add(new HeavyRaider(szektor));
+        else if (type == 4)
             baseStars.Add(new BaseStar(szektor));
         else
             Console.WriteLine("No such ship type!");
@@ -55,7 +62,7 @@ public class Board
 
     class Raider
     {
-        byte szektor;
+        public byte szektor;
 
         public Raider(byte szektor)
         {
@@ -65,7 +72,7 @@ public class Board
 
     class HeavyRaider
     {
-        byte szektor;
+        public byte szektor;
 
         public HeavyRaider(byte szektor)
         {
@@ -75,7 +82,7 @@ public class Board
 
     class Viper
     {
-        byte szektor;
+        public byte szektor;
 
         public Viper(byte szektor)
         {
@@ -85,7 +92,7 @@ public class Board
 
     class Civil
     {
-        byte szektor;
+        public byte szektor;
 
         public Civil(byte szektor)
         {
@@ -95,7 +102,7 @@ public class Board
 
     class BaseStar
     {
-        byte szektor;
+        public byte szektor;
 
         public BaseStar(byte szektor)
         {
@@ -103,4 +110,3 @@ public class Board
         }
     }
 }
-
